f1a07e4aa563816ea1bca78533cbf204df15de22
DurgaRaoUppalapati <durga.uppalapati@nxtwave.co.in>
2024-11-16 23:29:10 +0530

    Add admin

--- file_share/admin.py
+++ file_share/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from file_share import models
+
 
 # Register your models here.
+@admin.register(models.UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "username", "email", "role"]
+
+
+@admin.register(models.FileDetails)
+class FileDetailsAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "uploaded_by_id", "creation_datetime"]
